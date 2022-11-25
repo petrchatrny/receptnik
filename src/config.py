992ee8f2833439752cdccc45c5c5852fbaab7ade
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-11-25 18:11:23 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class BaseConfig(object):
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
